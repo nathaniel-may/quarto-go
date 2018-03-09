9cccc788f27144662cc93d63649e584d8194b33b
nathaniel may <nathanieldmay@gmail.com>
2018-03-08 20:54:05 -0500

    refactored board. added test for equality after one turn

--- quarto/quarto.go
+++ quarto/quarto.go
@@ -1,18 +1,31 @@
 package quarto
 
-import "errors"
+import (
+	"errors"
+	"strconv"
+)
+
+type Square struct {
+	H int
+	V int
+}
 
 type board struct {
 	id string
-	squares map[int]map[int]piece
-	pieces map[piece]bool
+	squares map[Square]Piece
+	pieces map[Piece]bool
 	lines map[string]int
-	active piece
+	active Piece
 }
 
 type Quarto interface {
 	isWon() bool
 	TakeTurn(toPlace piece, h int, v int, active piece) Quarto
+	Nil()
+}
+
+func (board *board) Nil() *board {
+	return NewBoard("nil")
 }
 
 func (board *board) isWon() bool{
@@ -20,33 +33,82 @@ func (board *board) isWon() bool{
 	return false
 }
 
-func (board *board) TakeTurn(toPlace piece, h int, v int, active piece) Quarto {
-	//TODO
-	return board
+func (board board) TakeTurn(toPlace Piece, h int, v int, active Piece) (board, error) {
+	if board.pieces[toPlace] == true {
+		return *board.Nil(), errors.New("the placed piece is already on the board")
+	}
+	if board.pieces[active] == true {
+		return *board.Nil(), errors.New("the active piece is already on the board")
+	}
+	if _, ok := board.squares[Square {h,v}]; ok {
+		return *board.Nil(), errors.New("the square is already occupied")
+	}
+	if board.active != Pieces.ZERO && board.active != toPlace {
+		return *board.Nil(), errors.New("must place the active piece")
+	}
+
+	board.squares[Square {h,v}] = toPlace
+	board.pieces[toPlace] = true
+	board.active = active
+	for _, line := range getLines(h, v) {
+		board.lines[line]++
+	}
+
+	return board, nil
 }
 
 func (board *board) Validate() (bool, error) {
-	if Pieces().ZERO == board.active && len(board.squares) != 0 && !board.isWon() {
+	if Pieces.ZERO == board.active && len(board.squares) != 0 && !board.isWon() {
 		return false, errors.New("active piece required")
 	}
 	//TODO other probs
 	return true, nil
 }
 
-func (board *board) setSquares(squares *map[int]map[int]piece) {
+func (board *board) setSquares(squares *map[Square]Piece) {
 	board.squares = *squares
 }
 
 func NewBoard(id string) *board {
-	return &board {id, map[int]map[int]piece {}, map[piece]bool {}, map[string]int {}, Pieces().ZERO}
+	return &board {id, make(map[Square]Piece), make(map[Piece]bool), make(map[string]int), Pieces.ZERO}
 }
 
-func InProgBoard(id string, squares *map[int]map[int]piece, active piece) *board {
+func InProgBoard(id string, squares *map[Square]Piece, active Piece) board {
 	board := NewBoard(id)
 	board.setSquares(squares)
-	//TODO use squares to set lines and pieces too
+	pieces := make(map[Piece]bool)
+	lines := make(map[string]int)
+	for square, piece := range *squares {
+		pieces[piece]=true
+		for _, line := range getLines(square.H, square.V) {
+			lines[line]++
+		}
+	}
+	board.pieces = pieces
+	board.lines = lines
+	board.active = active
 	board.Validate()
-	return board
+	return *board
+}
+
+func getLines(h int, v int) []string{
+	var lines []string
+	lines = append(lines, "h" + strconv.Itoa(h))
+	lines = append(lines, "v" + strconv.Itoa(v))
+
+	if h == v {
+		lines = append(lines, "d" + strconv.Itoa(h))
+	} else if h == 1 && v == 4 {
+		lines = append(lines, "d2")
+	} else if h == 2 && v == 3 {
+		lines = append(lines, "d2")
+	} else if h == 3 && v == 2 {
+		lines = append(lines, "d2")
+	} else if h == 4 && v == 1 {
+		lines = append(lines, "d2")
+	}
+
+	return lines
 }
 
 
@@ -57,34 +119,53 @@ type piece struct {
 	top string
 }
 
+type Piece interface {
+	getColor() string
+	getHeight() string
+	getShape() string
+	getTop() string
+}
+
+func (piece *piece) getColor() string{
+	return piece.color
+}
+
+func (piece *piece) getHeight() string{
+	return piece.height
+}
+
+func (piece *piece) getShape() string{
+	return piece.shape
+}
+
+func (piece *piece) getTop() string{
+	return piece.top
+}
+
 type pieces struct {
-	ZERO piece
-	WTQF, WTQH, WTRF, WTRH piece
-	WSQF, WSQH, WSRF, WSRH piece
-	BTQF, BTQH, BTRF, BTRH piece
-	BSQF, BSQH, BSRF, BSRH piece
+	ZERO Piece
+	WTQF, WTQH, WTRF, WTRH Piece
+	WSQF, WSQH, WSRF, WSRH Piece
+	BTQF, BTQH, BTRF, BTRH Piece
+	BSQF, BSQH, BSRF, BSRH Piece
 }
 
-func Pieces() *pieces {
-	return &p
-}
-
-var p = pieces{
-piece {"zero","zero","zero","zero"},
-piece {"white","tall","square","flat"},
-piece {"white","tall","square","hole"},
-piece {"white","tall","round","flat"},
-piece {"white","tall","round","hole"},
-piece {"white","short","square","flat"},
-piece {"white","short","square","hole"},
-piece {"white","short","round","flat"},
-piece {"white","short","round","hole"},
-piece {"black","tall","square","flat"},
-piece {"black","tall","square","hole"},
-piece {"black","tall","round","flat"},
-piece {"black","tall","round","hole"},
-piece {"black","short","square","flat"},
-piece {"black","short","square","hole"},
-piece {"black","short","round","flat"},
-piece {"black","short","round","hole"},
+var Pieces = pieces {
+		&piece {"zero","zero","zero","zero"},
+		&piece {"white","tall","square","flat"},
+		&piece {"white","tall","square","hole"},
+		&piece {"white","tall","round","flat"},
+		&piece {"white","tall","round","hole"},
+		&piece {"white","short","square","flat"},
+		&piece {"white","short","square","hole"},
+		&piece {"white","short","round","flat"},
+		&piece {"white","short","round","hole"},
+		&piece {"black","tall","square","flat"},
+		&piece {"black","tall","square","hole"},
+		&piece {"black","tall","round","flat"},
+		&piece {"black","tall","round","hole"},
+		&piece {"black","short","square","flat"},
+		&piece {"black","short","square","hole"},
+		&piece {"black","short","round","flat"},
+		&piece {"black","short","round","hole"},
 }
--- tests/quarto_test.go
+++ tests/quarto_test.go
@@ -1,8 +1,9 @@
 package tests
 
-import "../quarto"
-import "testing"
-import "reflect"
+import ("../quarto"
+		"testing"
+		"reflect"
+		"fmt")
 
 func TestBoardEquality(t *testing.T) {
 	if !reflect.DeepEqual(quarto.NewBoard("test"), quarto.NewBoard("test")) {
@@ -15,5 +16,23 @@ func TestValidate(t *testing.T) {
 	valid, error := board.Validate()
 	if !valid {
 		t.Errorf(error.Error())
+	}
+}
+
+func TestInProgEqualityOneTurn(t *testing.T) {
+	board := *quarto.NewBoard("test")
+	board, error := board.TakeTurn(quarto.Pieces.WTQF,2, 3, quarto.Pieces.BSQF)
+	if error != nil {
+		t.Errorf(error.Error())
+	}
+
+	squares := make(map[quarto.Square]quarto.Piece)
+	squares[quarto.Square{2,3}] = quarto.Pieces.WTQF
+	inProg := quarto.InProgBoard("test", &squares, quarto.Pieces.BSQF)
+
+	if !reflect.DeepEqual(board, inProg) {
+		fmt.Println(board)
+		fmt.Println(inProg)
+		t.Fail()
 	}
 }
