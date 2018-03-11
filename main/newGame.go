f2f7070c9917349d8161a7df898bbb8b7022947f
nathaniel may <nathanieldmay@gmail.com>
2018-03-11 13:28:36 -0400

    stubbed lambda and test

--- main/newGame.go
+++ main/newGame.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Lambda function handler
+func NewGameHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	//TODO use SNS to call the dal lambda function to create a new game
+
+	// If no name is provided in the HTTP request body, throw an error
+	if len(request.Body) < 1 {
+		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
+	}
+
+	//TODO return json representation of the game (new package)
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello " + request.Body,
+		StatusCode: 200,
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(NewGameHandler)
+}
--- main/newGame_test.go
+++ main/newGame_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"testing"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func TestNewGameHandler(t *testing.T) {
+	//TODO "goify" with input/expect pairs
+	var response events.APIGatewayProxyResponse
+	response, err := NewGameHandler(events.APIGatewayProxyRequest{})
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	//TODO STUB
+	if response.Body != "exactly what is expected" {
+		t.Fail()
+	}
+}
